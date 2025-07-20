3d4c56afdf92fd9cecfeaf68c19397f4964ee107
Vlada <geazint6@gmail.com>
2025-07-20 07:03:02 +0300

    first commit

---
+++ react-blended/src/task-01.ts
@@ -0,0 +1,20 @@
+// Є об'єкт settings, який містить налаштування.
+
+// const settings = {
+//   darkMode: true,
+//   fontSize: 16,
+//   language: "en",
+// };
+
+// Завдання:
+
+// Створіть тип Settings, який описує цей об'єкт.
+// Типізуйте settings через цей тип.
+
+const settings = {
+  darkMode:true,
+  fontSize: 16,
+  language: "en",
+};
+
+console.log(settings)
